4da659f110ab8bd4dc56cc55a4486873d7bb9279
Abdulrahman <113942757+Abman2@users.noreply.github.com>
2025-09-07 13:39:55 +0100

    Update HamburgerMenu.jsx
    Changes

--- src/component/HamburgerMenu.jsx
+++ src/component/HamburgerMenu.jsx
@@ -1,24 +1,24 @@
 import { Link, NavLink } from "react-router-dom";
 
 
-export const HamburgerMenu = () => {
+export const HamburgerMenu = ({setIsMenuOpen}) => {
 
   return (
     <ul
       className=' md:hidden z-10 fixed top-[90px] pt-5 left-0 mx-auto h-full w-full border-t-2 flex flex-col font-semibold text-[17px] space-y-7  text-center'
       style={{ backgroundColor: 'rgb(23, 24, 38)' }}>
       <li>
-        <NavLink to='security' className='hover:text-pink-600' >
+        <NavLink to='security' className='hover:text-pink-600' onClick={() => setIsMenuOpen(false)} >
           Security
         </NavLink>
       </li>
       <li>
-        <a href='/' className='hover:text-pink-600'>
+        <a href='/' className='hover:text-pink-600'onClick={() => setIsMenuOpen(false)}>
           Support
         </a>
       </li>
       <li>
-        <NavLink to="news" className='hover:text-pink-600'>
+        <NavLink to="news" className='hover:text-pink-600' onClick={() => setIsMenuOpen(false)}>
           News
         </NavLink>
       </li>
@@ -27,7 +27,7 @@ export const HamburgerMenu = () => {
           Swag
         </a>
       </li>
-      <NavLink to='downloadApp' className='mx-auto'>
+      <NavLink to='downloadApp' className='mx-auto' onClick={() => setIsMenuOpen(false)}>
         <button className='bg bg-pink-600 flex h-12 w-[182px]  items-center justify-center rounded-full  '>Download App</button>
       </NavLink>
     </ul>
